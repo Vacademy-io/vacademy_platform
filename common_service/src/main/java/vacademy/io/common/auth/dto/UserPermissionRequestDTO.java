a8efc8a7d6be942bf2db45ccd604c725fceaf558
PANKAJ DUBEY <147909642+PANKAJDUBEY2002@users.noreply.github.com>
2024-11-11 21:30:55 +0530

    take user in request attribute for non internal api (#2)

---
+++ common_service/src/main/java/vacademy/io/common/auth/dto/UserPermissionRequestDTO.java
@@ -0,0 +1,16 @@
+package vacademy.io.common.auth.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPermissionRequestDTO {
+
+    private String userId;
+    private String permissionId;
+
+}

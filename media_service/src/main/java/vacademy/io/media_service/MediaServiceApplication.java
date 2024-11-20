fe0d44f28f33af5d348d9fce5107bdeeefb980c2
Shreyash <shreyashjain1996@gmail.com>
2024-11-20 16:46:19 +0530

    auth trigger

--- media_service/src/main/java/vacademy/io/media_service/MediaServiceApplication.java
+++ media_service/src/main/java/vacademy/io/media_service/MediaServiceApplication.java
@@ -13,6 +13,7 @@ import vacademy.io.common.auth.config.SharedConfigurationReference;
 public class MediaServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MediaServiceApplication.class, args);
+
     }
 
 }

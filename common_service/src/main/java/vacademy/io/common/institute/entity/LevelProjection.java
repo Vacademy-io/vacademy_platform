faa8e895a60a66e1917d4d40cadd014bf445e756
Shreyash <shreyashjain1996@gmail.com>
2024-12-03 17:40:02 +0530

    added fix

--- common_service/src/main/java/vacademy/io/common/institute/entity/LevelProjection.java
+++ common_service/src/main/java/vacademy/io/common/institute/entity/LevelProjection.java
@@ -1,7 +1,11 @@
 package vacademy.io.common.institute.entity;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface LevelProjection {
         String getId();
+
+        @Value("#{target.level_name}")
         String getLevelName();
         Integer getDurationInDays();
     }

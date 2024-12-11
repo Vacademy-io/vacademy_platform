60ef734388dbecb9e4905459857fcce7a7bd344c
Shreyash <shreyashjain1996@gmail.com>
2024-12-11 12:33:27 +0530

    added assessment data

---
+++ assessment_service/src/main/java/vacademy/io/assessment_service/core/config/ApplicationSecurityConfig.java
@@ -0,0 +1,88 @@
+package vacademy.io.assessment_service.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfigurationSource;
+import vacademy.io.common.auth.filter.InternalAuthFilter;
+import vacademy.io.common.auth.filter.JwtAuthFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class ApplicationSecurityConfig {
+
+
+    private static final String[] INTERNAL_PATHS = {"/assessment-service/internal/**"};
+
+    private static final String[] ALLOWED_PATHS = { "/assessment-service/question-paper/upload/docx/v1/**","/assessment-service/actuator/**", "/assessment-service/swagger-ui.html", "/assessment-service/v1/report/alert/**", "/assessment-service/v3/api-docs/**", "/assessment-service/swagger-ui/**", "/assessment-service/webjars/swagger-ui/**", "/assessment-service/api-docs/**"};
+
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    InternalAuthFilter internalAuthFilter;
+
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(ALLOWED_PATHS).permitAll()
+                .requestMatchers(INTERNAL_PATHS).authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(internalAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
---
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/upload_docx/dto/QuestionResponseFromDocx.java
@@ -0,0 +1,44 @@
+package vacademy.io.assessment_service.features.upload_docx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionResponseFromDocx {
+    private int questionId;
+    private String questionHtml;
+    private List<OptionResponseFromDocx> optionsData = new ArrayList<>();
+    private List<String> answerOptionIds = new ArrayList<>();
+    private String explanationHtml;
+    private List<String> errors = new ArrayList<>();
+    private List<String> warnings = new ArrayList<>();
+
+    public void addOption(OptionResponseFromDocx option) {
+        this.optionsData.add(option);
+    }
+
+    public void setQuestionHtml(String html) {
+        this.questionHtml = html;
+    }
+
+    public void appendQuestionHtml(String html) {
+        this.questionHtml = (this.questionHtml == null ? "" : this.questionHtml) + html;
+    }
+
+    public void appendExplanationHtml(String html) {
+        this.explanationHtml = (this.explanationHtml == null ? "" : this.explanationHtml) + html;
+    }
+
+    public QuestionResponseFromDocx(int questionId) {
+        this.questionId = questionId;
+    }
+
+}

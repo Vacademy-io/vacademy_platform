ba2920254e209aeab90c82c11c4caaa51d492d37
Shreyash <shreyashjain1996@gmail.com>
2024-12-17 16:21:30 +0530

    added mix data

--- assessment_service/src/main/java/vacademy/io/assessment_service/features/question_bank/repository/QuestionPaperRepository.java
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/question_bank/repository/QuestionPaperRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import vacademy.io.assessment_service.features.question_bank.dto.QuestionPaperDTO;
 import vacademy.io.assessment_service.features.question_bank.entity.QuestionPaper;
 
 import java.util.List;
@@ -26,16 +27,19 @@ public interface QuestionPaperRepository extends JpaRepository<QuestionPaper, St
     @Query(value = "INSERT INTO institute_question_paper (id, question_paper_id, institute_id, status, level_id, subject_id) VALUES (:id, :questionPaperId, :instituteId, :status, :levelId, :subjectId)", nativeQuery = true)
     void linkInstituteToQuestionPaper(@Param("id") String id, @Param("questionPaperId") String questionPaperId, @Param("instituteId") String instituteId, @Param("status") String status, @Param("levelId") String levelId, @Param("subjectId") String subjectId);
 
-
     @Query(
-            value = "SELECT qp.* FROM question_paper qp " +
+            value = "SELECT qp.id AS id, qp.title AS title, iq.status AS status, " +
+                    "iq.level_id AS levelId, iq.subject_id AS subjectId, " +
+                    "qp.created_on AS createdOn, " +
+                    "qp.updated_on AS updatedOn, qp.created_by_user_id AS createdByUserId " +
+                    "FROM question_paper qp " +
                     "LEFT JOIN institute_question_paper iq ON qp.id = iq.question_paper_id " +
                     "WHERE (:title IS NULL OR qp.title ILIKE CONCAT('%', :title, '%')) " +
                     "AND (:status IS NULL OR iq.status IN (:status)) " +
                     "AND (:levelIds IS NULL OR iq.level_id IN (:levelIds)) " +
                     "AND (:subjectIds IS NULL OR iq.subject_id IN (:subjectIds)) " +
                     "AND (:instituteIds IS NULL OR iq.institute_id IN (:instituteIds)) " +
-                    "AND (:createdByUserId IS NULL OR qp.created_by_user_id = :createdByUserId) ",
+                    "AND (:createdByUserId IS NULL OR qp.created_by_user_id = :createdByUserId)",
             countQuery = "SELECT COUNT(qp) FROM question_paper qp " +
                     "LEFT JOIN institute_question_paper iq ON qp.id = iq.question_paper_id " +
                     "WHERE (:title IS NULL OR qp.title ILIKE CONCAT('%', :title, '%')) " +
@@ -43,10 +47,10 @@ public interface QuestionPaperRepository extends JpaRepository<QuestionPaper, St
                     "AND (:levelIds IS NULL OR iq.level_id IN (:levelIds)) " +
                     "AND (:subjectIds IS NULL OR iq.subject_id IN (:subjectIds)) " +
                     "AND (:instituteIds IS NULL OR iq.institute_id IN (:instituteIds)) " +
-                    "AND (:createdByUserId IS NULL OR qp.created_by_user_id = :createdByUserId) ",
+                    "AND (:createdByUserId IS NULL OR qp.created_by_user_id = :createdByUserId)",
             nativeQuery = true
     )
-    Page<QuestionPaper> findQuestionPapersByFilters(
+    Page<Object[]> findQuestionPapersByFilters(
             @Param("title") String title,
             @Param("status") List<String> status,
             @Param("levelIds") List<String> levelIds,
@@ -55,6 +59,7 @@ public interface QuestionPaperRepository extends JpaRepository<QuestionPaper, St
             @Param("instituteIds") List<String> instituteIds,
             Pageable pageable
     );
+
 
     @Modifying
     @Transactional

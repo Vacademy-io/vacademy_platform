d47b6465405c3e42fe8ae6a7a34fa9f98b2dd353
Shreyash <shreyashjain1996@gmail.com>
2024-12-21 10:33:51 +0530

    added changes to operation api

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/student/dto/StudentStatusUpdateRequest.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/student/dto/StudentStatusUpdateRequest.java
@@ -1,5 +1,8 @@
 package vacademy.io.admin_core_service.features.student.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +14,8 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StudentStatusUpdateRequest {
     private String userId;
     private String newState;

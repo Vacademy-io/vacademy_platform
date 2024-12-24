ef411ffb25504d0028bb84d87706fa61618ab682
Punit Punde <153176922+punitpunde@users.noreply.github.com>
2024-12-24 15:41:02 +0530

    Feature/files (#7)
    * api created to upload, delete and getting the files to the user
    
    * bug fix in delete file
    
    * changed response to multiple files from file ids
    
    * changed request mpping from /media to /media_service
    
    * apis created for media-service/internal

--- media_service/src/main/java/vacademy/io/media_service/constant/MediaConstant.java
+++ media_service/src/main/java/vacademy/io/media_service/constant/MediaConstant.java
@@ -3,3 +3,4 @@ package vacademy.io.media_service.constant;
 public class MediaConstant {
     public static String s3baseurl = "https://laborlink-media-bucket.s3-accelerate.amazonaws.com/";
 }
+// Need to fix

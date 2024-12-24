ab36e570f5915386c2a88f2b4d3bbb8cd91211b7
Punit Punde <punitpunde@gmail.com>
2024-12-24 10:19:52 +0530

    changed response to multiple files from file ids

--- media_service/src/main/java/vacademy/io/media_service/dto/UserToFileDTO.java
+++ media_service/src/main/java/vacademy/io/media_service/dto/UserToFileDTO.java
@@ -12,7 +12,7 @@ import vacademy.io.common.media.dto.FileDetailsDTO;
 public class UserToFileDTO {
     private String userId;
     private FileDetailsDTO fileDetail;
-    private FileDetailsDTO folderIconDetail;
+    private String folderIconUrl;
     private String userid;
     private String folderName;
     private String sourceId;
--- media_service/src/main/java/vacademy/io/media_service/service/UserToFileService.java
+++ media_service/src/main/java/vacademy/io/media_service/service/UserToFileService.java
@@ -1,13 +1,13 @@
 package vacademy.io.media_service.service;
 
 import vacademy.io.media_service.dto.UserToFileDTO;
-import vacademy.io.media_service.entity.UserToFile;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserToFileService {
     List<UserToFileDTO>getUserFilesByUserId(String userId);
-    String deleteFileByFileId(String fileId);
-    List<UserToFileDTO>getUserFilesByFolderAndUserId(String folderName,String userId);
-    UserToFileDTO getUserFile(String userId, String fileId);
+    String deleteFilesByFileIds(String fileIds);
+    Map<String, List<UserToFileDTO>>getUserFilesByFoldersAndUserId(String folderNames, String userId);
+    List<UserToFileDTO> getUserFiles(String userId, String fileIds);
 }

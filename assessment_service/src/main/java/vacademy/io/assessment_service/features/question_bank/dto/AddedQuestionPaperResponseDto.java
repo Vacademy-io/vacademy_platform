75c32b30485da071341ed5808861c9c2d67a1ec3
Shreyash <shreyashjain1996@gmail.com>
2025-01-02 12:42:19 +0530

    added steps to assessments

---
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/question_bank/dto/AddedQuestionPaperResponseDto.java
@@ -0,0 +1,17 @@
+package vacademy.io.assessment_service.features.question_bank.dto;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@AllArgsConstructor
+public class AddedQuestionPaperResponseDto {
+
+    private String savedQuestionPaperId;
+}
---
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/question_core/enums/SubmissionTypes.java
@@ -0,0 +1,5 @@
+package vacademy.io.assessment_service.features.question_core.enums;
+
+public enum SubmissionTypes {
+   VIDEO_LIVE_RECORD, VIDEO_LIVE_UPLOAD, PDF, FILE
+}

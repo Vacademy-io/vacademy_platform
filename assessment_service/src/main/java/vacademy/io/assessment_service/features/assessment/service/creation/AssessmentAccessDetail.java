5b1580881d8067b1c49cd0dddce43636a64781f7
Shreyash <shreyashjain1996@gmail.com>
2025-01-10 18:26:15 +0530

    added changes

--- assessment_service/src/main/java/vacademy/io/assessment_service/features/assessment/service/creation/AssessmentAccessDetail.java
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/assessment/service/creation/AssessmentAccessDetail.java
@@ -1,11 +1,14 @@
 package vacademy.io.assessment_service.features.assessment.service.creation;
 
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 import vacademy.io.assessment_service.features.assessment.entity.Assessment;
+import vacademy.io.assessment_service.features.assessment.entity.AssessmentInstituteMapping;
 import vacademy.io.assessment_service.features.assessment.enums.StepStatus;
 import vacademy.io.assessment_service.features.assessment.enums.creationSteps.AccessControlEnum;
 import vacademy.io.assessment_service.features.assessment.service.IStep;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -23,7 +26,21 @@ public class AssessmentAccessDetail extends IStep {
 
     @Override
     public void checkStatusAndFetchData(Optional<Assessment> assessment) {
-        setStatus(StepStatus.INCOMPLETE.name());
+        setStatus(StepStatus.COMPLETED.name());
+        if (assessment.isEmpty()) return;
+        Optional<AssessmentInstituteMapping> assessmentInstituteMapping = getAssessmentUrlByInstituteIdAndAssessmentId(getInstituteId(), assessment.get());
+        if (assessmentInstituteMapping.isEmpty()) return;
+        Map<String, Object> savedData = new HashMap<>();
+        savedData.put(AccessControlEnum.CREATION_ACCESS.name().toLowerCase(), Pair.of(assessmentInstituteMapping.get().getCommaSeparatedCreationUserIds(), assessmentInstituteMapping.get().getCommaSeparatedCreationRoles()));
+        savedData.put(AccessControlEnum.EVALUATION_ACCESS.name().toLowerCase(), Pair.of(assessmentInstituteMapping.get().getCommaSeparatedEvaluationUserIds(), assessmentInstituteMapping.get().getCommaSeparatedEvaluationRoles()));
+        savedData.put(AccessControlEnum.LIVE_ASSESSMENT_ACCESS.name().toLowerCase(), Pair.of(assessmentInstituteMapping.get().getCommaSeparatedLiveViewUserIds(), assessmentInstituteMapping.get().getCommaSeparatedLiveViewRoles()));
+        savedData.put(AccessControlEnum.REPORT_AND_SUBMISSION_ACCESS.name().toLowerCase(), Pair.of(assessmentInstituteMapping.get().getCommaSeparatedSubmissionViewUserIds(), assessmentInstituteMapping.get().getCommaSeparatedSubmissionViewRoles()));
+        setSavedData(savedData);
+    }
+
+    private Optional<AssessmentInstituteMapping> getAssessmentUrlByInstituteIdAndAssessmentId(String instituteId, Assessment assessment) {
+        return assessment.getAssessmentInstituteMappings().stream().filter(
+                assessmentInstituteMapping -> assessmentInstituteMapping.getAssessment().equals(assessment) && assessmentInstituteMapping.getInstituteId().equals(instituteId)).findFirst();
     }
 
     @Override

a0d0200d9100b9260fe9d018d17ad97eb711563c
Punit Punde <punitpunde@gmail.com>
2025-01-14 19:58:10 +0530

    added notification service

---
+++ notification_service/src/main/java/vacademy/io/notification_service/controller/EmailController.java
@@ -0,0 +1,49 @@
+package vacademy.io.notification_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vacademy.io.common.notification.dto.EmailOTPRequest;
+import vacademy.io.notification_service.dto.EmailRequest;
+import vacademy.io.notification_service.features.email_otp.service.OTPService;
+import vacademy.io.notification_service.service.EmailService;
+
+@RestController
+@RequestMapping("notification-service/v1")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    private final OTPService otpService;
+
+    @Autowired
+    public EmailController(EmailService emailService, OTPService otpService) {
+        this.emailService = emailService;
+        this.otpService = otpService;
+    }
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestBody EmailRequest request) {
+        emailService.sendEmail(request.getTo(), request.getSubject(), request.getText());
+
+        return "Email sent successfully";
+    }
+
+    @PostMapping("/verify-email-otp")
+    public ResponseEntity<String> verifyEmailOtp(@RequestBody EmailOTPRequest request) {
+        if (otpService.verifyEmailOtp(request.getOtp(), request.getTo()))
+            return ResponseEntity.ok("Email Verified Successfully");
+        throw new RuntimeException("Invalid OTP");
+    }
+
+    @PostMapping("/send-email-otp")
+    public ResponseEntity<String> sendEmailOtp(@RequestBody EmailOTPRequest request) {
+        otpService.sendEmailOtp(request.getTo(), request.getSubject(), request.getService(), request.getName());
+        return ResponseEntity.ok("Email OTP sent successfully");
+    }
+
+
+}

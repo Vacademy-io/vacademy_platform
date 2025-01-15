c0c293609deaa196743a6d4c036dda03692eec88
Punit Punde <153176922+punitpunde@users.noreply.github.com>
2025-01-16 05:24:02 +0530

    added notification service (#11)
    * added notification service
    
    * added notification service
    
    ---------
    
    Co-authored-by: Shreyash <shreyashjain1996@gmail.com>

---
+++ notification_service/src/main/java/vacademy/io/notification_service/exceptions/FileDownloadException.java
@@ -0,0 +1,7 @@
+package vacademy.io.notification_service.exceptions;
+
+public class FileDownloadException extends Exception {
+    public FileDownloadException(String message) {
+        super(message);
+    }
+}

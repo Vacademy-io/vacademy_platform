f46bfd865f9494a2030fae40c77d2baa321437f7
Shreyash <shreyashjain1996@gmail.com>
2025-01-22 13:18:39 +0530

    fixed auth issue

---
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/student_assessment/controller/StudentAssessmentGetController.java
@@ -0,0 +1,10 @@
+package vacademy.io.assessment_service.features.student_assessment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/assessment-service/assessment/learner")
+public class StudentAssessmentGetController {
+
+}

8fc7cb7228ec0457e45f10a504cf3056cef00467
Punit Punde <punitpunde@gmail.com>
2025-02-05 14:50:52 +0530

    added hmac

---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/notification/dto/EmailDTO.java
@@ -0,0 +1,4 @@
+package vacademy.io.admin_core_service.features.notification.dto;
+
+public class EmailDTO {
+}
---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/notification/service/NotificationService.java
@@ -0,0 +1,14 @@
+package vacademy.io.admin_core_service.features.notification.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vacademy.io.common.core.internal_api_wrapper.InternalClientUtils;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private InternalClientUtils internalClientUtils;
+
+
+}

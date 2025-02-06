c1ff501530bfecb0d2345f889cb4a877a93d9ad7
Punit Punde <punitpunde@gmail.com>
2025-02-07 03:25:58 +0530

    changed internal assess controller

--- assessment_service/src/main/java/vacademy/io/assessment_service/features/learner_assessment/controller/LearnerAssessmentInternalController.java
+++ assessment_service/src/main/java/vacademy/io/assessment_service/features/learner_assessment/controller/LearnerAssessmentInternalController.java
@@ -1,10 +1,9 @@
-package vacademy.io.assessment_service.features.assessment.controller;
+package vacademy.io.assessment_service.features.learner_assessment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vacademy.io.assessment_service.features.assessment.manager.AssessmentParticipantsManager;
-import vacademy.io.common.auth.model.CustomUserDetails;
 
 @RestController
 @RequestMapping("/assessment-service/internal/assessment/learner-assessment/v1")
@@ -15,7 +14,6 @@ public class LearnerAssessmentInternalController {
 
     @GetMapping("/assessment-count-for-user-id")
     public ResponseEntity<Integer> getAssessmentCountForUserId(@RequestParam String userId,@RequestParam String instituteId) {
-        System.out.println("getAssessmentCountForUserId");
         return ResponseEntity.ok(assessmentParticipantsManager.getAssessmentCountForUserId(userId, instituteId));
     }
 }

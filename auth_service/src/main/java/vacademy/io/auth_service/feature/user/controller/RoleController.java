a75f904ed8c2a532e1427140e7082fadab955c9d
Punit Punde <punitpunde@gmail.com>
2025-02-07 16:33:18 +0530

    api added to add roles to user

--- auth_service/src/main/java/vacademy/io/auth_service/feature/user/controller/RoleController.java
+++ auth_service/src/main/java/vacademy/io/auth_service/feature/user/controller/RoleController.java
@@ -1,14 +1,27 @@
 package vacademy.io.auth_service.feature.user.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vacademy.io.auth_service.feature.user.dto.AddRolesToUserDTO;
 import vacademy.io.auth_service.feature.user.service.RoleService;
+import vacademy.io.common.auth.model.CustomUserDetails;
 
 @RestController
+@RequestMapping("auth-service/v1/user-roles")
 public class RoleController {
 
-    @Autowired
-    RoleService roleService;
+    private final RoleService roleService;
 
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/add-roles-to-user")
+    public ResponseEntity<String> addRolesToUser(
+            @RequestBody AddRolesToUserDTO addRolesToUserDTO,
+            @RequestAttribute("user") CustomUserDetails customUserDetails) {
+
+        String response = roleService.addRolesToUser(addRolesToUserDTO, customUserDetails);
+        return ResponseEntity.ok(response);
+    }
 }

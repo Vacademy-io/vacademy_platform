c0470ed8ec40145e09da80c8fc9a91626d2ea126
Manshu48 <manshujaiswar02@gmail.com>
2026-04-28 15:19:00 +0530

    fix default queue to be annotated as primary

--- notification_service/src/main/java/vacademy/io/notification_service/config/AwsSqsConfig.java
+++ notification_service/src/main/java/vacademy/io/notification_service/config/AwsSqsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -23,6 +24,7 @@ public class AwsSqsConfig {
     @Value("${aws.region}")
     private String region;
 
+    @Primary
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
